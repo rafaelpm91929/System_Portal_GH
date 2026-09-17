be7526c7bac3c29ffee21a9e59aa6b11973499c3
Rafael <rpm91929@gmail.com>
2026-09-17 13:50:56 -0600

    feat(config): agregar config_env.php visible para asegurar sincronizacion por FTP/Git

---
+++ config_env.php
@@ -0,0 +1,9 @@
+<?php
+// Archivo de Configuración de Credenciales MySQL para cPanel - Divol La Villa
+// Puedes editar las credenciales directamente aquí si cambian en tu cPanel
+
+putenv("MYSQL_HOST=localhost");
+putenv("MYSQL_DB=divolavi_sistemas");
+putenv("MYSQL_USER=divolavi_user");
+putenv("MYSQL_PASS=SecuredPass2026!");
+?>
